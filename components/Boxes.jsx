79ccadd6f6980b09dae0f337fdf431a804f594b8
bishir tukur <bastukur1997@gmail.com>
2023-11-20 11:02:49 +0100

    layout

---
+++ components/Boxes.jsx
@@ -0,0 +1,28 @@
+import "../components/Boxes.css";
+
+
+const Boxes = () =>{
+    return(
+      <>
+    <main className="flex-container">
+
+      <section className="main-container">
+       <h2>Main content</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus ad temporibus. Modi consequatur iusto ullam voluptate nemo corporis tempora laborum quasi repudiandae ab distinctio, fuga, dolores tenetur repellat laudantium.
+       </p>
+      </section>
+
+    <aside className="sidebar">
+    <h2>Sidebar</h2>
+     <p>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Est provident voluptatem sit voluptatum maxime? Officia suscipit totam facilis perferendis beatae blanditiis officiis asperiores consequatur corrupti, enim quasi cupiditate quae molestiae!
+     </p>
+    </aside>
+
+    </main>
+      </>
+    )
+  };
+  
+  export default Boxes;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Boxes from "../components/Boxes";
+
+
+
+const App = () =>{
+  return(
+    <>
+    <Boxes/>
+    </>
+  )
+};
+
+export default App;
